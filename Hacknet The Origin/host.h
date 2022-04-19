4b7a75288473338a9bb7f2d6859e7fa4c132b558
闻浩 隼 <sunwenhao181@outlook.com>
2022-04-19 17:43:54 +0800

    更新：完善host类

--- Hacknet The Origin/host.h
+++ Hacknet The Origin/host.h
@@ -5,8 +5,8 @@ class host
 public:
 	host(unsigned char* p, unsigned char* m, bool w);
 	std::string hostname;
-	unsigned char ip[4]={"0"};
-	unsigned char mac[6]={"0"};
+	std::string ip = "0.0.0.0";
+	std::string mac = "00:00:00:00:00:00";
 	bool status[8] = { false }; //0 ssh 1 FTP 2 WEB 3 HTTPS 4 SSL(Vulnable) 5 Bittorrent 6 SMTP (vulnable) 7 SMB
 	bool vulnstatus[8] = { false };  //0 ssh 1 FTP 2 WEB 3 HTTPS 4 SSL 5 Bittorrent 6 SMTP 7 SMB
 	bool icmpable=true;
