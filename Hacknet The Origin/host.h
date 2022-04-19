86ffa914fde90852ef78604a608600fa2a0ee881
闻浩 隼 <sunwenhao181@outlook.com>
2022-04-19 17:18:08 +0800

    更新：完善host类

--- Hacknet The Origin/host.h
+++ Hacknet The Origin/host.h
@@ -10,8 +10,10 @@ public:
 	bool status[8] = { false }; //0 ssh 1 FTP 2 WEB 3 HTTPS 4 SSL(Vulnable) 5 Bittorrent 6 SMTP (vulnable) 7 SMB
 	bool vulnstatus[8] = { false };  //0 ssh 1 FTP 2 WEB 3 HTTPS 4 SSL 5 Bittorrent 6 SMTP 7 SMB
 	bool icmpable=true;
+	void(*connectedfunc)();
 	void(*hackedfunc)();
 	void(*attackedfunc)();
+	void(*disconnectfunc);
 };
 
 
